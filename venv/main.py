73a8d033e9112c1caab03394b4b7891eaaffd991
nenab2501 <hsb2@rice.edu>
2022-09-09 15:42:58 -0500

    added initial app

---
+++ venv/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
+#to run
+#cd into s4/venv
+#. /bin/activate
+#flask --app main run

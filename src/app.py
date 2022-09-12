e77ca0ee266fe9629e4b604154c80228e68ff9b0
Eric Yang <spacecube9801@gmail.com>
2022-09-12 15:56:36 -0500

    preliminary changes

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ api = Api(
 
 ns = api.namespace("S4", description="S4 API Endpoints")
 
+# API Model for example header/body and the response
 key = api.model(
     "Key",
     {
@@ -25,16 +26,28 @@ key = api.model(
     },
 )
 
+# quick question: do we need a separate class for all of the endpoints?
 
+# Endpoints
 @ns.route("/generateKey")
 class generateKey(Resource):
     """Route for generating a new API key"""
 
     @ns.doc("generateKey")
-    @api.response(200, "Success", model=key)
+    @api.response(200, "Success", model=key)         # response.status_code returned?
     def get(self):
         return {"Key": secrets.token_urlsafe(nbytes=16)}
 
-
+# @ns.route("/getObject", methods=['GET'])
+# class getObject():
+#     def get(self):
+#         return "hello world"
+    
+# @ns.route("/putObject", methods=['PUT'])
+# class getObject():
+#     def put(self):
+#         return "meow"
+    
+# Main
 if __name__ == "__main__":
     app.run(debug=True)

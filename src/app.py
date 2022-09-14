1cbe243f0307f33713c01a11e4b6256c819ae0d0
Eric Yang <spacecube9801@gmail.com>
2022-09-14 15:48:18 -0500

    preliminary changes:

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+import secrets, os
+
+from flask import Flask, request, redirect, flash, url_for
 from flask_restx import Api, Resource, fields
+
 from werkzeug.middleware.proxy_fix import ProxyFix
-import secrets
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -26,28 +29,56 @@ key = api.model(
     },
 )
 
-# quick question: do we need a separate class for all of the endpoints?
+UPLOAD_FOLDER = '/Users/ericy/Desktop'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 # Endpoints
 @ns.route("/generateKey")
 class generateKey(Resource):
-    """Route for generating a new API key"""
-
     @ns.doc("generateKey")
     @api.response(200, "Success", model=key)         # response.status_code returned?
     def get(self):
         return {"Key": secrets.token_urlsafe(nbytes=16)}
 
 # @ns.route("/getObject", methods=['GET'])
-# class getObject():
+# class getObject(Resource):
+#     @ns.doc("getObject")
+#     @api.response(200, "Success", model=key)         # response.status_code returned?
 #     def get(self):
-#         return "hello world"
-    
-# @ns.route("/putObject", methods=['PUT'])
-# class getObject():
-#     def put(self):
-#         return "meow"
-    
+#         return {"Key1": secrets.token_urlsafe(nbytes=16)}
+
+# @ns.route("/putFile", methods=['PUT'])
+# class uploadFile(Resource):
+#     def allowed_file(filename):
+#         return '.' in filename and \
+#             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#     @ns.doc("putFile")
+#     @api.response(200, "Success", model=key)
+#     def upload_file():
+#         if request.method == 'PUT':
+#             if 'file' not in request.files:
+#                 flash('No file part')
+#                 return redirect(request.url)
+#             file = request.files['file']
+            
+#             if file.filename == '':
+#                 flash('No selected file')
+#                 return redirect(request.url)
+#             if file and allowed_file(file.filename):
+#                 filename = secure_filename(file.filename)
+#                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#                 return redirect(url_for('download_file', name=filename))
+#         return '''
+#         <!doctype html>
+#         <title>Upload new File</title>
+#         <h1>Upload new File</h1>
+#         <form method=post enctype=multipart/form-data>
+#         <input type=file name=file>
+#         <input type=submit value=Upload>
+#         </form>
+#         '''
+
 # Main
 if __name__ == "__main__":
     app.run(debug=True)

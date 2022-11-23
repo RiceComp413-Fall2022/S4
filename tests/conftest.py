f9546ed96482bbd00c1355f0ad01e54b1d846384
nenab2501 <hsb2@rice.edu>
2022-11-22 21:16:29 -0600

    updated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import json
 import threading
 from werkzeug.serving import make_server
 import requests
+from multiprocessing import Process
 
 from worker.app import app as worker_app
 from main.src.app import app as main_app
@@ -12,7 +13,7 @@ class ServerThread(threading.Thread):
 
     def __init__(self, app, port):
         threading.Thread.__init__(self)
-        self.server = make_server("127.0.0.1", port, app)
+        self.server = make_server("127.0.0.1", port, app, threaded=True)
         self.ctx = app.app_context()
         self.ctx.push()
 
@@ -29,18 +30,19 @@ def start_server():
     #Start up three workers
 
     ports = [5001, 5002, 5003]
-    workers = []
-    for i in range(len(ports)):
-        worker = ServerThread(worker_app, ports[i])
-        worker.start()
-        workers.append(worker)
+    # workers = []
+    # for i in range(len(ports)):
+    #     worker = ServerThread(worker_app, ports[i])
+    #     worker.start()
+    #     workers.append(worker)
 
     #Start up a main node
     main_url = "http://127.0.0.1:5000/"
-    main_node = ServerThread(main_app, 5000)
-    main_node.start()
-
-    #Set up worker nodes
+    # main_node = ServerThread(main_app, 5000)
+    # main_node.start()
+    # r = requests.get(main_url + "HealthCheck")
+    # print(r.status_code)
+    # Set up worker nodes
     ALL_WORKERS = ["http://127.0.0.1:" + str(port) + "/" for port in ports]
     for i in range(len(ports)):
         url = "http://127.0.0.1:" + str(ports[i]) + "/_SetWorkers"
@@ -52,7 +54,7 @@ def start_server():
                 })
 
     print('server started')
-    yield main_node
-    main_node.shutdown()
-    for worker in workers:
-        worker.shutdown()
+    yield main_url
+    # main_node.shutdown()
+    # for worker in workers:
+    #     worker.shutdown()

6cdc0a29fdebfb8bd4724387a127897c4da1445b
gavin2059 <zz66@rice.edu>
2022-11-27 02:52:54 -0600

    nodes grid

--- dashboard/src/NodeStats.jsx
+++ dashboard/src/NodeStats.jsx
@@ -1,6 +1,32 @@
-import { Box } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 function NodeStats() {
-  return <Box minH="50vh" bgColor="#eebd8b"></Box>;
+  return (
+    <Box bgColor="#ffeebd">
+      <SimpleGrid columns={[1, 2, 3, 4]} px="5%" py="2%">
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+        <NodeStat />
+      </SimpleGrid>
+    </Box>
+  );
+}
+function NodeStat() {
+  return (
+    <Box w="100%" px="5%" py="2%">
+      <Box rounded="lg" bgColor="#eebd8b" style={{ aspectRatio: 1 }}>
+        asdfasdf
+      </Box>
+    </Box>
+  );
 }
 
 export default NodeStats;
